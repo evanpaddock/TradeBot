f1bf3a3531bd211936fe37d2e95e5c3b386fa713
Evan Paddock <edpaddock@crimson.ua.edu>
2024-09-23 21:54:34 -0500

    final starter template

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from schwab.orders.equities import equity_buy_market
 
 if __name__ == "__main__":
     load_dotenv()
-    
+
     client = client_from_login_flow(
         os.getenv("APP_KEY"),
         os.getenv("APP_SECRET"),
         os.getenv("CALLBACK_URL"),
-        "./token.json",
+        os.getenv("TOKEN_URL"),
     )
