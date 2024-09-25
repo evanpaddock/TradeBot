bc27e5292867056f3b7833cc4f7f7ba79d1e5a28
Evan Paddock <edpaddock@crimson.ua.edu>
2024-09-25 14:09:19 -0500

    acount works, streaming added

--- Notification.py
+++ Notification.py
@@ -3,34 +3,32 @@ import smtplib
 from email.mime.text import MIMEText
 
 
-class Notification:
-    # Function to send SMS via email
-    def send_sms_via_email(
-        message,
-        phone_number=os.getenv("PHONE_NUMBER"),
-        carrier_gateway=os.getenv("CARRIER_GATEWAY"),
-        sender_email=os.getenv("SENDER_EMAIL"),
-        sender_email_password=os.getenv("SENDER_EMAIL_PASSWORD"),
-        host=os.getenv("HOST"),
-        subject="Trade Notification",
-    ):
-        # Construct the email
-        to_number = f"{phone_number}@{carrier_gateway}"
-        msg = MIMEText(message)
-        msg["From"] = sender_email
-        msg["To"] = to_number
-        msg["Subject"] = subject
+def send_sms_via_email(
+    message,
+    phone_number=os.getenv("PHONE_NUMBER"),
+    carrier_gateway=os.getenv("CARRIER_GATEWAY"),
+    sender_email=os.getenv("SENDER_EMAIL"),
+    sender_email_password=os.getenv("SENDER_EMAIL_PASSWORD"),
+    host=os.getenv("HOST"),
+    subject="Trade Notification",
+):
+    # Construct the email
+    to_number = f"{phone_number}@{carrier_gateway}"
+    msg = MIMEText(message)
+    msg["From"] = sender_email
+    msg["To"] = to_number
+    msg["Subject"] = subject
 
-        try:
-            # Connect to the SMTP server
-            server = smtplib.SMTP(host=host, port=587)
-            server.starttls()
-            server.login(sender_email, sender_email_password)
+    try:
+        # Connect to the SMTP server
+        server = smtplib.SMTP(host=host, port=587)
+        server.starttls()
+        server.login(sender_email, sender_email_password)
 
-            # Send the email
-            server.sendmail(sender_email, to_number, msg.as_string())
-            print("SMS sent successfully!")
-        except Exception as e:
-            print(f"Failed to send SMS: {e}")
-        finally:
-            server.quit()
+        # Send the email
+        server.sendmail(sender_email, to_number, msg.as_string())
+        print("SMS sent successfully!")
+    except Exception as e:
+        print(f"Failed to send SMS: {e}")
+    finally:
+        server.quit()
--- app.py
+++ app.py
@@ -1,14 +1,48 @@
-from dotenv import load_dotenv
 import os
-from schwab.auth import client_from_login_flow
-from schwab.orders.equities import equity_buy_market
+from dotenv import load_dotenv
+from schwab.auth import *
+from schwab.orders import *
+from schwab.streaming import *
+import asyncio
+
+
+async def main():
+    client = get_client()
+    data = await get_account(client)
+    account_hash = data.json()[0]["hashValue"]
+    stream_client = StreamClient(client, account_id=account_hash)
+    await read_stream(stream_client)
+
+
+async def get_account(client):
+    data = await client.get_account_numbers()
+    return data
+
+
+def get_client():
+    api_key = os.getenv("APP_KEY")
+    app_secret = os.getenv("APP_SECRET")
+    callback_url = os.getenv("CALLBACK_URL")
+    token_path = os.getenv("TOKEN_PATH")
+
+    return easy_client(api_key, app_secret, callback_url, token_path, asyncio=True)
+
+
+async def read_stream(stream_client):
+    await stream_client.login()
+
+    def print_message(message):
+        print(json.dumps(message, indent=4))
+
+    # Always add handlers before subscribing because many streams start sending
+    # data immediately after success, and messages with no handlers are dropped.
+    stream_client.add_nasdaq_book_handler(print_message)
+    await stream_client.nasdaq_book_subs(["SPY"])
+
+    while True:
+        await stream_client.handle_message()
+
 
 if __name__ == "__main__":
-    load_dotenv()
-
-    client = client_from_login_flow(
-        os.getenv("APP_KEY"),
-        os.getenv("APP_SECRET"),
-        os.getenv("CALLBACK_URL"),
-        os.getenv("TOKEN_URL"),
-    )
+    load_dotenv(".env")
+    asyncio.run(main())
