d51216f882d1da72fe2de7eedb693dc5a939eaf5
Evan Paddock <edpaddock@crimson.ua.edu>
2024-09-26 21:44:14 -0500

    big changes

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 import os
 from dotenv import load_dotenv
-from schwab.auth import *
-from schwab.orders import *
-from schwab.streaming import *
-import asyncio
+from schwab.auth import easy_client, Client
+
+from models import Order
 
 
-async def main():
+def setup():
     client = get_client()
-    data = await get_account(client)
-    account_hash = data.json()[0]["hashValue"]
-    stream_client = StreamClient(client, account_id=account_hash)
-    await read_stream(stream_client)
+    account_hash = get_account_hash(client)
+    return (client, account_hash)
 
 
-async def get_account(client):
-    data = await client.get_account_numbers()
-    return data
+def get_account_hash(client: Client) -> any:
+    accountHash = client.get_account_numbers().json()[0]["hashValue"]
+    return accountHash
 
 
 def get_client():
@@ -25,24 +22,11 @@ def get_client():
     callback_url = os.getenv("CALLBACK_URL")
     token_path = os.getenv("TOKEN_PATH")
 
-    return easy_client(api_key, app_secret, callback_url, token_path, asyncio=True)
-
-
-async def read_stream(stream_client):
-    await stream_client.login()
-
-    def print_message(message):
-        print(json.dumps(message, indent=4))
-
-    # Always add handlers before subscribing because many streams start sending
-    # data immediately after success, and messages with no handlers are dropped.
-    stream_client.add_nasdaq_book_handler(print_message)
-    await stream_client.nasdaq_book_subs(["SPY"])
-
-    while True:
-        await stream_client.handle_message()
+    return easy_client(api_key, app_secret, callback_url, token_path)
 
 
 if __name__ == "__main__":
     load_dotenv(".env")
-    asyncio.run(main())
+    client, account_hash = setup()
+    # Order.buy_order(client, account_hash, "F", 1)
+    Order.cancel_all_open_orders(client, account_hash)
